cd6812645d61fb9a8afd1b8fa7b41c3ee96e1e3b
FlekGeKei <skleimenov19@gmail.com>
2022-05-19 20:09:10 +0300

    19052022

---
+++ Linux/Games/Saints Row The Third/start.sh
@@ -0,0 +1,2 @@
+cd /home/flekgekei/Games/Saints\ Row\ The\ Third/
+wine SaintsRowTheThird_DX11.exe
---
+++ Linux/Games/osu!lazer/updater.sh
@@ -0,0 +1,4 @@
+cd ~/osu\!/
+rm osu.AppImage 
+wget https://github.com/ppy/osu/releases/latest/download/osu.AppImage
+chmod +x osu.AppImage
